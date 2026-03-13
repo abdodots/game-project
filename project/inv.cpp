c68dda104cef6569e7118a2a4ea9def7254c63dd
mttrr5555-beep <mttrr5555@gmail.com>
2026-03-14 01:32:03 +0200

    Refactor inventory class structure and methods

--- project/inv.cpp
+++ project/inv.cpp
@@ -1,1 +1,46 @@
 #include "inv.h"
+#include <iostream>
+
+using namespace std;
+
+inventory::inventory()
+{
+  inv_size = 100; // المساحة الأساسية للانفينتوري
+}
+
+void inventory::set_size(int size)
+{
+  inv_size = size;
+}
+
+bool inventory::check(int size)
+{
+  if (inv_size + size >= 0)
+    return true;
+  else
+    return false;
+}
+
+void inventory::add_item(int size)
+{
+  if (check(size))
+  {
+    inv_size += size;
+    cout << "Item added. Inventory size: " << inv_size << endl;
+  }
+  else
+  {
+    cout << "Inventory full!" << endl;
+  }
+}
+
+void inventory::delet_item(int size)
+{
+  inv_size -= size;
+  cout << "Item deleted. Inventory size: " << inv_size << endl;
+}
+
+void inventory::get_item(int item)
+{
+  cout << "Item number " << item << " selected." << endl;
+}
--- project/inv.h
+++ project/inv.h
@@ -1,5 +1,25 @@
-#pragma once
-class inv
+#ifndef INVENTORY_H
+#define INVENTORY_H
+
+class inventory
 {
+private:
+    int inv_size;
+
+public:
+    inventory();
+
+    void set_size(int size);
+
+    void get_item(int item);
+
+    bool check(int size);
+
+    void add_item(int size);
+
+    void delet_item(int size);
 };
+
+#endif
+
 
