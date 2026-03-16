8e436dc65b4f16a66fb87e36dad1ac200602cb02
mohamedashraf29909 <mohamedashraf29909@gmail.com>
2026-03-16 15:40:39 +0200

    system

--- project/system.h
+++ project/system.h
@@ -5,14 +5,58 @@
 #include "items.h"
 using namespace std;
 
-class GameSystem
+class GameSystem : public charecter 
 {
 public:
   // فانكشن لعرض حالة الشخصية
+  void showStats(string name, int hp, int st, int sp);
+
+  // Alias for possible typo "howStats"
   void showStats(string name, int hp, int st, int sp);
 
   // تشيك لو الشخصية لسه عايشة
   bool isAlive(int health) { return health > 0; }
 
   int showMenu();
+
+  int basic_Attack();
+
+  void sp_po(int x);
+  void take_damage(int x);
+  void set_stamina();
+};
+
+
+class GameSystemInventory : public inventory
+{
+public:
+	void get_items();
+};
+
+class GameSystemPotion : public potion
+{
+public:
+	int healing();
+	int size();
+};
+
+class GameSystemPoison : public poison
+{
+public:
+	int damage();
+	int size();
+};
+
+class GameSystemShield : public shield
+{
+public:
+	void set_health(int x);
+	int size();
+};
+
+class GameSystemWeapon : public weapon
+{
+public:
+	int heavy_attack();
+	int melee_attack();
 };
